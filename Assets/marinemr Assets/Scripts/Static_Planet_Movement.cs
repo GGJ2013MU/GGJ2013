50f9d826e7c316d24f6d978ac141ffd13fb2d224
Matthew Dwyer <dwyermw@muohio.edu>
2013-01-26 13:57:40 -0500

    Enemy now moves, however...
    Sinks into the ground.  Trying collision detection with the ground.

--- Assets/marinemr Assets/Scripts/Static_Planet_Movement.cs
+++ Assets/marinemr Assets/Scripts/Static_Planet_Movement.cs
@@ -19,6 +19,7 @@ public class Static_Planet_Movement : MonoBehaviour {
 		rigidbody.velocity += (direction.normalized * gravityAccel);
 		
 		rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxSpeed);
+		
 	}
 	
 	void rotationAdj(){
@@ -26,6 +27,24 @@ public class Static_Planet_Movement : MonoBehaviour {
 		//transform.rotation = rot;
 		transform.LookAt(new Vector3(0,transform.position.y, 0));
 		transform.Rotate(new Vector3(-90,0,0));
+	}
+	
+	void OnCollisionEnter(Collision collision){
+		if(collision.gameObject.tag == "Platform"){
+			grounded = true;
+			rigidbody.velocity += transform.up.normalized * 2;
+		}
+		
+		//if(collision.gameObject.tag == "Enemy"){
+			// call method jumped for EnemyInterface
+		//	if(collision.gameObject.GetComponent<Enemy2>().jumped()){
+				
+		//		Vector3 direction = new Vector3(0,0,0) - transform.position;
+		//		rigidbody.velocity -= direction.normalized;
+					
+				//Destroy(collision.gameObject);
+			//}
+	//	}
 	}
 	
 	// movement
