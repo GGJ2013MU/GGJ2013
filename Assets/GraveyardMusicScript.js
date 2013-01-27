1bd4bf201b35c87159927ec4eef8cfffc72d0c55
Matthew Marine <holy.maniac@gmail.com>
2013-01-27 06:26:40 -0500

    Music files
    Added music files

---
+++ Assets/GraveyardMusicScript.js
@@ -0,0 +1,16 @@
+#pragma strict
+
+@script RequireComponent(AudioClip)
+
+// 1 second = 44100 samples, delay requires samples
+var delay = 22050;
+
+function Start () {
+	
+}
+
+function FixedUpdate () {
+
+	//audio.Play();
+	
+}
